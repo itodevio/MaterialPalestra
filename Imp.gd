030dfb64b1f37e16fbaa14563b668de428c9248d
Italo Aguiar <italoaa99@gmail.com>
2019-10-03 01:45:12 -0300

    final update

--- Imp.gd
+++ Imp.gd
@@ -5,19 +5,11 @@ signal shoot
 export (PackedScene) var Bullet
 
 var can_shoot = true
-
-func _ready():
-	add_to_group("turrets")
-
+	
 func shoot():
 	var dir = Vector2(1, 0).rotated(global_rotation)
 	emit_signal("shoot", Bullet, $Position2D.global_position, dir)
 		
-
 func _on_AnimatedSprite_frame_changed():
 	if $AnimatedSprite.animation == "attack" and $AnimatedSprite.frame == 3:
 		shoot()		
-
-
-func _on_Timer_timeout():
-	pass # Replace with function body.
